0c7245b2c9e9f7d05709f0da7b81d04fd62ab15f
DaffyDug <hanzi1609@icloud.com>
2024-03-25 20:15:30 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DraverHelper.Avalible();
+
+
+        DriveInfo[] driveInfos = DriveInfo.GetDrives();
+        foreach (var item in driveInfos)
+        {
+            if (item.IsReady)
+            {
+                Console.WriteLine(item.TotalFreeSpace.ToGb());
+                Console.WriteLine(item.RootDirectory);
+            }
+            if (Directory.Exists($@"{item.RootDirectory}\Windows"))
+            {
+                Console.WriteLine("такая папка есть");
+            }
+            else
+            {
+                Console.WriteLine("такой папки нет");
+            }
+        }
+    }
+}
+public static class TOGB
+{
+    public static long ToGb(this long b)
+    {
+        return b / 1000000000;
+    }
+}
+
+public static class DraverHelper
+{
+    public static void Avalible()
+    {
+        int procent;
+        DriveInfo[] driveInfos = DriveInfo.GetDrives();
+        foreach (var item in driveInfos)
+        {
+            if (item.IsReady)
+            {
+                procent = (int)((float)item.TotalFreeSpace / (float)item.TotalSize * 100);
+                Console.WriteLine($"{item.Name} {procent}%");
+            }
+        }
+    }
+}
